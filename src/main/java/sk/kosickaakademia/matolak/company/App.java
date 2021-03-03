5cda7bc9cc6d02ee890873859fd5a77ca49787c3
Adam Matoľák <adam.matolak@kosickaakademia.sk>
2021-03-03 11:15:21 +0100

    Added insertNewUser function

--- src/main/java/sk/kosickaakademia/matolak/company/App.java
+++ src/main/java/sk/kosickaakademia/matolak/company/App.java
@@ -1,12 +1,14 @@
 package sk.kosickaakademia.matolak.company;
 
 import sk.kosickaakademia.matolak.company.database.Database;
+import sk.kosickaakademia.matolak.company.entity.User;
+import sk.kosickaakademia.matolak.company.enumerator.Gender;
 
 public class App 
 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         Database db=new Database();
-        db.getConnection();
+        db.insertNewUser(new User("Roman","Banik",35, 0));
     }
 }
--- src/main/java/sk/kosickaakademia/matolak/company/database/Database.java
+++ src/main/java/sk/kosickaakademia/matolak/company/database/Database.java
@@ -1,5 +1,6 @@
 package sk.kosickaakademia.matolak.company.database;
 
+import sk.kosickaakademia.matolak.company.entity.User;
 import sk.kosickaakademia.matolak.company.log.Log;
 
 import java.io.IOException;
@@ -11,17 +12,17 @@ import java.util.Properties;
 
 public class Database {
 
-    String url = "jdbc:mysql://itsovy.sk:3306/world_x";
-    String username="mysqluser";
-    String password="Kosice2021!";
-
     Log log = new Log();
+    private final String INSERTQUERY ="INSERT INTO user (fname, lname, age, gender) " +
+            " VALUES (?, ?, ?, ?)";
     public Connection getConnection(){
         try {
             Properties props = new Properties();
             InputStream loader = getClass().getClassLoader().getResourceAsStream("db.properties");
             props.load(loader);
-
+            String url = props.getProperty("url");
+            String username=props.getProperty("username");
+            String password=props.getProperty("password");
             Connection con = DriverManager.getConnection(url, username, password);
             log.print("Connection: success!");
             return con;
@@ -34,9 +35,29 @@ public class Database {
         if(con!=null){
             try{
                 con.close();
+                log.print("connection closed");
             }catch (SQLException e){
                 log.error(e.toString());
             }
         }
+    }
+    public boolean insertNewUser(User user){
+        Connection con = getConnection();
+        if (con!=null){
+            try {
+                PreparedStatement ps = con.prepareStatement(INSERTQUERY);
+                ps.setString(1,user.getFname());
+                ps.setString(2,user.getLname());
+                ps.setInt(3,user.getAge());
+                ps.setInt(4,user.getGender().getValue());
+                int result = ps.executeUpdate();
+                closeConnection(con);
+                log.print("New user has been added to the DB");
+                return result==1;
+            }catch (SQLException ex){
+                log.error(ex.toString());
+            }
+        }
+        return false;
     }
 }
